8bd4100e1dc4c1944719abd4925559f46c05097d
BirkhoffLee <github@birkhoff.me>
2017-06-20 07:17:02 +0800

    fix "syntax"

--- src/chunky.sh
+++ src/chunky.sh
@@ -10,5 +10,5 @@ java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar --update
 echo "${prefix}Downloading Minecraft ${MINECRAFT_VERSION}....$suffix"
 java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar -download-mc $MINECRAFT_VERSION
 
-echo "${prefix}Rendering.... (Ctrl-C to force stop)$suffix"
+echo "${prefix}Running.... (Ctrl-C to force stop)$suffix"
 exec java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar $@
