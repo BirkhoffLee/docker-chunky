e9b526567ac44aca2d42b5483fd3c62446e63fdf
BirkhoffLee <github@birkhoff.me>
2017-06-20 07:00:08 +0800

    add friendly notices

--- src/chunky.sh
+++ src/chunky.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+bold=$(tput bold)
+suffix=$(tput sgr0)
+prefix="$bold== "
+
+echo "${prefix}Updating Chunky....$suffix"
 java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar --update
+
+echo "${prefix}Downloading Minecraft ${MINECRAFT_VERSION}....$suffix"
 java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar -download-mc $MINECRAFT_VERSION
+
+echo "${prefix}Rendering.... (Ctrl-C to force stop)$suffix"
 exec java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar $@
