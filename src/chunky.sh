e46bc92841ebd95da9a2f56f5a827047e3d0f7d5
BirkhoffLee <github@birkhoff.me>
2017-06-20 06:40:47 +0800

    test

--- src/chunky.sh
+++ src/chunky.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+exec java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar --update
+exec java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar -download-mc $MINECRAFT_VERSION
 exec java -Dchunky.home="/data" $JAVA_ARGS -jar /srv/ChunkyLauncher.jar $@
